f627ac1ad469645cf0c0bee3ca5771b670e8eddb
MarioEsPe <mario_espinosap@yahoo.com>
2025-10-14 08:50:43 -0600

    feat: Add PostgreSQL support for production environments

--- app/database.py
+++ app/database.py
@@ -1,11 +1,14 @@
 # app/database.py
+import os
 from sqlmodel import create_engine, Session
 
-sqlite_file_name =  "database.db"
+sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args = connect_args)
+
+database_url = os.getenv("DATABASE_URL", default=sqlite_url)
+
+engine = create_engine(database_url)
 
 def get_session():
-    with Session(engine) as session: 
+    with Session(engine) as session:
         yield session
