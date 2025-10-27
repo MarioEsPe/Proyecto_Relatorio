c642e4888612868c8592fcca20e2249a6e86f7b8
MarioEsPe <mario_espinosap@yahoo.com>
2025-10-26 21:13:52 -0600

    fix(api): configure CORS middleware to allow frontend requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # app/main.py 
 from fastapi import FastAPI 
 from sqlmodel import SQLModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.routers import equipment, shifts, users, login, personnel, attendance, tank, license, tasks, parameters, maintenance
@@ -9,6 +10,20 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine) 
     
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",      
+    "http://localhost:5174",      
+    # "https://relatorio-ui.onrender.com", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      
+    allow_credentials=True,   
+    allow_methods=["*"],      
+    allow_headers=["*"],      
+)
 
 @app.on_event("startup")
 def on_startup():
