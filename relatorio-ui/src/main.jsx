a9cd3b58d3bcee51118b303f4b33f68d908fc7e8
MarioEsPe <mario_espinosap@yahoo.com>
2025-10-27 10:36:42 -0600

    feat(ui): configure react query client provider

--- relatorio-ui/src/main.jsx
+++ relatorio-ui/src/main.jsx
@@ -1,10 +1,20 @@
+// relatorio-ui/src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// --- 1. Importar React Query ---
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// --- 2. Crear el Cliente ---
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* --- 3. Envolver la App --- */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
